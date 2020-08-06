7f2f1458f2a1012a2ca635805e3eeca51ea6f1df
Alexandru Marinescu <ext.amarinescu@kds.com>
2020-08-06 15:29:16 +0300

    Here are my initial changes.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace TrainingGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is Alex Marinescu.");
         }
     }
 }
