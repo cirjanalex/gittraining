c36d55170f4341f6f9c861d1edfe81f6f0574e6e
Alexandru Cirjan <cirjan@mds.microsoft.com>
2020-08-06 15:19:19 +0300

    Created a new console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrainingGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
